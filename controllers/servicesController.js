8c9185f1ff9176a4eb12ec7334b4208a681a4856
Robbie <saucy.cooking@gmail.com>
2019-01-25 18:16:08 -0700

    added boilerplate stuff

---
+++ controllers/servicesController.js
@@ -0,0 +1,11 @@
+const express       = require('express');
+const router        = express.Router();
+const Users         = require('../models/User');
+const Events        = require('../models/Event');
+const Services      = require('../models/Service');
+
+
+
+
+
+module.exports = router;
