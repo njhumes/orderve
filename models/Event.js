e60aeb1499aee172c128ede8428261809aec5e2b
Nicholas Humes <njhumes@gmail.com>
2019-01-26 17:00:44 -0700

    Completed Events Controller, Routes, Model, and EJS files

--- models/Event.js
+++ models/Event.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
+const Services = require('./Service')
+const Users = require('./User')
 
+const eventSchema = mongoose.Schema({
+    title: {type: String, require: true},
+    location: {type: String, require: true},
+    date: {type: Date, require: true},
+    description: {type: String, require: true},
+    // services: [Services.schema], // TBD if we need this imported or if this will just be a drop down
+    // host: [Users.schema],
+    budget: Number,
+})
+
+const Event = mongoose.model('Event', eventSchema)
+module.exports = Event;
 
