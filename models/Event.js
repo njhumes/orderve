ef0d8c07f3212d7302f7badc932a31c40535dd16
Nicholas Humes <njhumes@gmail.com>
2019-01-28 12:37:28 -0700

    updated routes with connectivity to other models

--- models/Event.js
+++ models/Event.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const Services = require('./Service')
-const Users = require('./User')
+const User = require('./User')
 
 const eventSchema = mongoose.Schema({
     title: {type: String, require: true},
     location: {type: String, require: true},
     date: {type: Date, require: true},
     description: {type: String, require: true},
-    // services: [Services.schema], // TBD if we need this imported or if this will just be a drop down
-    // host: [Users.schema],
+    services: [Services.schema], // TBD if we need this imported or if this will just be a drop down
+    // host: [User.schema],
     budget: Number,
 })
 
